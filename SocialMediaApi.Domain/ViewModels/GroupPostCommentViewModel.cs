667de260f3bde85f2e329845918c4a3e47a783fa
Welcome Sithole <sitholewb@gmail.com>
2023-06-13 21:02:08 +0200

    Add IGroupPostCommentService

---
+++ SocialMediaApi.Domain/ViewModels/GroupPostCommentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SocialMediaApi.Domain.Entities.Base;
+using SocialMediaApi.Domain.Entities.JsonEntities;
+
+namespace SocialMediaApi.Domain.ViewModels
+{
+    public class GroupPostCommentViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid GroupPostId { get; set; }
+        public BaseUser Creator { get; set; } = new BaseUser();
+        public string Text { get; set; } = string.Empty;
+        public int Downloads { get; set; } = 0;
+        public int Views { get; set; } = 0;
+        public int Rank { get; set; } = 0;
+        public int TotalComments { get; set; } = 0;
+        public Reaction Reactions { get; set; } = new Reaction { };
+        public Media Media { get; set; } = new Media { };
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset LastModifiedDate { get; set; }
+        public DateTimeOffset ActionBasedDate { get; set; }
+    }
+}
