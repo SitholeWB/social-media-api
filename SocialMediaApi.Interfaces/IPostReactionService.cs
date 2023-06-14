11c8b5259205ed277c63be1a4cff6e8f7a625b51
Welcome Sithole <sitholewb@gmail.com>
2023-06-14 21:11:14 +0200

    Add IPostReactionService

---
+++ SocialMediaApi.Interfaces/IPostReactionService.cs
@@ -0,0 +1,12 @@
+﻿using SocialMediaApi.Domain.Models.Reactions;
+using SocialMediaApi.Domain.ViewModels;
+
+namespace SocialMediaApi.Interfaces
+{
+    public interface IPostReactionService
+    {
+        public Task<ReactionViewModel> AddReactionAsync(AddReactionModel model);
+
+        public Task DeleteReactionAsync(Guid entityId, Guid id);
+    }
+}
