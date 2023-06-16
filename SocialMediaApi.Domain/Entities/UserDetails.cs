620f7c9d87f070b78ed2aa13632b0d9c1972560f
Welcome Sithole <sitholewb@gmail.com>
2023-06-16 16:45:15 +0200

    Add UserDetails entity

---
+++ SocialMediaApi.Domain/Entities/UserDetails.cs
@@ -0,0 +1,16 @@
+﻿using SocialMediaApi.Domain.Entities.JsonEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaApi.Domain.Entities
+{
+    public class UserDetails
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public List<MiniReaction> CommentReactions { get; set; } = new List<MiniReaction>();
+        public List<MiniReaction> PostReactions { get; set; } = new List<MiniReaction>();
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset LastModifiedDate { get; set; }
+    }
+}
