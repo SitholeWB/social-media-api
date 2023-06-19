3946241db241fa07fa5cdca9f5e489b4bfd0784b
Welcome Sithole <sitholewb@gmail.com>
2023-06-19 20:00:43 +0200

    Adsd FeedController

---
+++ SocialMediaApi/Controllers/FeedController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pagination.EntityFrameworkCore.Extensions;
+using SocialMediaApi.Domain.ViewModels;
+using SocialMediaApi.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SocialMediaApi.Controllers
+{
+    [Authorize]
+    [Route("api/v1/feed")]
+    [ApiController]
+    public class FeedController : ControllerBase
+    {
+        private readonly IFeedService _feedService;
+
+        public FeedController(IFeedService feedService)
+        {
+            _feedService = feedService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Pagination<PostViewModel>>> GetPostsAsync(int page = 1, int limit = 20)
+        {
+            return Ok(await _feedService.GetPostsAsync(page, limit));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("trending")]
+        public async Task<ActionResult<Pagination<PostViewModel>>> GetTrendingPostsAsync(int page = 1, int limit = 20)
+        {
+            return Ok(await _feedService.GetTrendingPostsAsync(page, limit));
+        }
+    }
+}
