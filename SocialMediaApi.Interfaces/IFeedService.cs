a5482c01f65b9dcc6b79a37c83557b2764c832a9
Welcome Sithole <sitholewb@gmail.com>
2023-06-19 11:37:35 +0200

    Add IFeedService

---
+++ SocialMediaApi.Interfaces/IFeedService.cs
@@ -0,0 +1,12 @@
+﻿using Pagination.EntityFrameworkCore.Extensions;
+using SocialMediaApi.Domain.ViewModels;
+
+namespace SocialMediaApi.Interfaces
+{
+    public interface IFeedService
+    {
+        public Task<Pagination<PostViewModel>> GetPostsAsync(int page, int limit);
+
+        public Task<Pagination<PostViewModel>> GetTrendingPostsAsync(int page, int limit);
+    }
+}
