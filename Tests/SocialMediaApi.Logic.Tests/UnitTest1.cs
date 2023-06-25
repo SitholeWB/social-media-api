8be8f15d94cd1334f1b24ffba321862ebed65861
Welcome Sithole <sitholewb@gmail.com>
2023-06-25 15:42:07 +0200

    Add Unit tests project

---
+++ Tests/SocialMediaApi.Logic.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SocialMediaApi.Logic.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
