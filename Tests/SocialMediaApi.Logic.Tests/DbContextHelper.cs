2f89b7f4ba537d515a41d3990d7d7800a37b8953
Welcome Sithole <sitholewb@gmail.com>
2023-06-25 15:55:22 +0200

    add DbContextHelper on Tests

---
+++ Tests/SocialMediaApi.Logic.Tests/DbContextHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaApi.Data;
+
+namespace SocialMediaApi.Logic.Tests
+{
+    public class DbContextHelper
+    {
+        public static SocialMediaApiDbContext GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<SocialMediaApiDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new SocialMediaApiDbContext(options);
+            return databaseContext;
+        }
+    }
+}
