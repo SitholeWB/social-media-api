a97d001937283565bba84b82030f093068c87a63
Welcome Sithole <sitholewb@gmail.com>
2023-11-19 13:29:49 +0200

    upgrade to .Net 8

--- SocialMediaApi.Domain/Common/HashingUtils.cs
+++ SocialMediaApi.Domain/Common/HashingUtils.cs
@@ -1,19 +1,18 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-namespace SocialMediaApi.Domain.Common
+namespace SocialMediaApi.Domain.Common;
+
+public struct HashingUtils
 {
-    public struct HashingUtils
+    public static string HashUserPassword(Guid userId, string inputString)
     {
-        public static string HashUserPassword(Guid userId, string inputString)
-        {
-            var combined = new StringBuilder();
-            combined.Append(userId.ToString().ToLowerInvariant());
-            combined.Append('_');
-            combined.Append(inputString);
-            using var sha512 = SHA512.Create();
-            using var fileStream = new MemoryStream(Encoding.UTF8.GetBytes(combined.ToString()));
-            return BitConverter.ToString(sha512.ComputeHash(fileStream)).Replace("-", "");
-        }
+        var combined = new StringBuilder();
+        combined.Append(userId.ToString().ToLowerInvariant());
+        combined.Append('_');
+        combined.Append(inputString);
+        using var sha512 = SHA512.Create();
+        using var fileStream = new MemoryStream(Encoding.UTF8.GetBytes(combined.ToString()));
+        return BitConverter.ToString(sha512.ComputeHash(fileStream)).Replace("-", "");
     }
 }
