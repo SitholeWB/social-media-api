9565c022e7d597aea471047235d4ef3545d18b4b
Welcome Sithole <sitholewb@gmail.com>
2026-01-02 00:21:29 +0200

    add updated migrations

---
+++ SocialMedia.Infrastructure/Migrations/20260101221523_comment_title_read.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class comment_title_read : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "CommentReads",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "CommentReads");
+        }
+    }
+}
--- SocialMedia.Infrastructure/Migrations/SocialMediaDb/20260101221610_comment_title_read.Designer.cs
+++ SocialMedia.Infrastructure/Migrations/SocialMediaDb/20260101221610_comment_title_read.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialMedia.Infrastructure;
 
@@ -11,9 +12,11 @@ using SocialMedia.Infrastructure;
 namespace SocialMedia.Infrastructure.Migrations.SocialMediaDb
 {
     [DbContext(typeof(SocialMediaDbContext))]
-    partial class SocialMediaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260101221610_comment_title_read")]
+    partial class comment_title_read
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
